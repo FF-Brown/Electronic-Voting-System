10ee8a03815bfde7af73ec145a556f3bf1436d9b
Nathan_B <54860453+FF-Brown@users.noreply.github.com>
2021-04-29 20:29:18 -0700

    Add initial files and framework for EMS.
    Created project, declared most functions for EMS, created empty files for most related classes.

---
+++ Electronic_Voting_System/Electronic_Voting_System/ElectionManagementSystem.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Electronic_Voting_System
+{
+    public class ElectionManagementSystem
+    {
+        private Election election;
+
+        private List<User> users;
+
+        private List<User> pendingValidations;
+
+        private List<Candidate> electionResults;
+
+        private User currentUser;
+
+        public void AuthenticateUser(User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DisplayAdminPortal()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<User> GetUserList()
+        {
+            return this.users;
+        }
+
+        public void RegisterUser(User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<User> GetPendingValidations()
+        {
+            return this.pendingValidations;
+        }
+
+        public void Vote()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Dictionary<string, int> GetDemographics()
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
