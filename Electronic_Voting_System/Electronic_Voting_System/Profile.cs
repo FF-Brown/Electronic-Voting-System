94f0d39e0265f59d50260b29c2d9d901dff54c2e
a-stromme <78772033+a-stromme@users.noreply.github.com>
2021-05-01 16:09:23 -0700

    Add Profile and User classes

--- Electronic_Voting_System/Electronic_Voting_System/Profile.cs
+++ Electronic_Voting_System/Electronic_Voting_System/Profile.cs
@@ -8,5 +8,35 @@ namespace Electronic_Voting_System
 {
     public class Profile
     {
+        // Name, Address, Email, Password, Date of Birth, Social Security Number
+		// private variables
+		private string name, address, email, pw, dob, ssn;
+
+		// Constructor 
+		public Profile()
+		{ 
+			name = "";		// Name
+			address = ""; 	// Address
+			email = "";		// Email
+			pw = "";		// Password
+			dob = "";		// Date of Birth
+			ssn = "";		// Social Security Number
+		}
+
+		// Getters
+		public string getName()		{	return name;	}
+		public string getAddress()	{	return address;	}
+		public string getEmail()	{	return email;	}
+		public string getPW()		{	return pw;		}
+		public string getDOB()		{	return dob;		}
+		public string getSSN()		{	return ssn;		}
+
+		// Setters
+		public void setName(string n )			{	name = n;		}
+		public void setAddress(string a)		{	address = a;	}
+		public void setEmail(string e)			{	email = e;		}
+		public void setPW(string p)				{	pw = p;			}
+		public void setDOB(string d)			{	dob = d;		}
+		public void setSSN(string p)			{	ssn = s;		}
     }
 }
--- Electronic_Voting_System/Electronic_Voting_System/User.cs
+++ Electronic_Voting_System/Electronic_Voting_System/User.cs
@@ -8,5 +8,61 @@ namespace Electronic_Voting_System
 {
     public class User
     {
+        // private variables
+
+		// Profile
+		Profile userProfile;
+		// Is the user already registered?
+		bool isRegistered;
+		// Has the user voted?
+		bool hasVoted;
+		// Is the user an administrator?
+		bool isAdmin;
+		// the candidate the user voted for
+		Candidate votedCandidate; 
+
+		// Constructor
+		public User()
+		{
+			userProfile = new Profile();
+			isRegistered = false;
+			hasVoted = false;
+			isAdmin = false;
+			Candidate votedCandidate = new Candidate();
+		}
+
+		/******** Getters ********/ 
+
+		// return whether the user is an administrator
+		public bool getIsAdmin(void)				{ return isAdmin; }
+
+		// return whether the user has voted
+		public bool getHasVoted(void)				{ return hasVoted;}
+
+		// return whether the user is registered 
+		public bool getIsRegistered(void)			{ return isRegistered; }
+
+		// return the Candidate the user voted for
+		public Candidate getVotedCandidate(void)	{ return votedCandidate; }
+
+		// return the user's profile
+		public Profile getUserProfile(void)			{ return userProfile; }
+
+		/******** Setters ********/ 
+
+		// set admin
+		public void setAdmin(bool b) { isAdmin = b; }
+
+		// set has voted
+		public void setHasVoted(bool b) { hasVoted = b; }
+
+		// set registered 
+		public void setIsRegistered(bool b) { isRegistered = b; }
+
+		// set the candidate the user voted for
+		public void setVotedCandidate (Candidate c) { Candidate c; }
+
+		// set the user's profile to a new profile
+		public void setProfile (Profile p) { userProfile = p; }
     }
 }
