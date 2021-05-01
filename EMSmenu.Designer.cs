6abd84bf8ce63b903a2d66170f29038d0a012fc7
MichaelJMcNaughton <78771630+MichaelJMcNaughton@users.noreply.github.com>
2021-04-30 18:32:34 -0700

    Add files via upload

---
+++ EMSmenu.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class EMSmenu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.GoToRegister = new System.Windows.Forms.Button();
+            this.GoToLogin = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // GoToRegister
+            // 
+            this.GoToRegister.Location = new System.Drawing.Point(127, 64);
+            this.GoToRegister.Name = "GoToRegister";
+            this.GoToRegister.Size = new System.Drawing.Size(139, 67);
+            this.GoToRegister.TabIndex = 0;
+            this.GoToRegister.Text = "Go To Register menu";
+            this.GoToRegister.UseVisualStyleBackColor = true;
+            this.GoToRegister.Click += new System.EventHandler(this.GoToRegister_Click);
+            // 
+            // GoToLogin
+            // 
+            this.GoToLogin.Location = new System.Drawing.Point(127, 177);
+            this.GoToLogin.Name = "GoToLogin";
+            this.GoToLogin.Size = new System.Drawing.Size(139, 66);
+            this.GoToLogin.TabIndex = 1;
+            this.GoToLogin.Text = "Go To Login Menu";
+            this.GoToLogin.UseVisualStyleBackColor = true;
+            this.GoToLogin.Click += new System.EventHandler(this.GoToLogin_Click);
+            // 
+            // EMSmenu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(405, 315);
+            this.Controls.Add(this.GoToLogin);
+            this.Controls.Add(this.GoToRegister);
+            this.Name = "EMSmenu";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "EMSmenu";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button GoToRegister;
+        private System.Windows.Forms.Button GoToLogin;
+    }
+}
---
+++ LoginMenu.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class LoginMenu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.LoginButton = new System.Windows.Forms.Button();
+            this.LeaveButtonL = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // LoginButton
+            // 
+            this.LoginButton.Location = new System.Drawing.Point(161, 228);
+            this.LoginButton.Name = "LoginButton";
+            this.LoginButton.Size = new System.Drawing.Size(137, 47);
+            this.LoginButton.TabIndex = 0;
+            this.LoginButton.Text = "Login";
+            this.LoginButton.UseVisualStyleBackColor = true;
+            this.LoginButton.Click += new System.EventHandler(this.LoginButton_Click);
+            // 
+            // LeaveButtonL
+            // 
+            this.LeaveButtonL.Location = new System.Drawing.Point(161, 55);
+            this.LeaveButtonL.Name = "LeaveButtonL";
+            this.LeaveButtonL.Size = new System.Drawing.Size(137, 50);
+            this.LeaveButtonL.TabIndex = 1;
+            this.LeaveButtonL.Text = "Go To EMS Menu";
+            this.LeaveButtonL.UseVisualStyleBackColor = true;
+            this.LeaveButtonL.Click += new System.EventHandler(this.LeaveButton_Click);
+            // 
+            // LoginMenu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(506, 361);
+            this.Controls.Add(this.LeaveButtonL);
+            this.Controls.Add(this.LoginButton);
+            this.Name = "LoginMenu";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Login Menu";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button LoginButton;
+        private System.Windows.Forms.Button LeaveButtonL;
+    }
+}
+
